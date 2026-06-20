407c23734eb60717b3d33043b3d66804d2accff5
Tuan Anh <tuanballbooo@gmail.com>
2026-06-20 12:04:56 +0700

    fix bug cors

--- src/services/api/api.js
+++ src/services/api/api.js
@@ -2,9 +2,16 @@
 import axios from "axios";
 import { API_BASE_URL } from "../../utils/apiEndpoint.js";
 
+const isNgrokUrl = /\.ngrok(-free)?\./i.test(API_BASE_URL);
+
 const api = axios.create({
   baseURL: API_BASE_URL,
   timeout: 10000,
+  headers: isNgrokUrl
+    ? {
+        "ngrok-skip-browser-warning": "true",
+      }
+    : undefined,
 });
 
 export default api;
